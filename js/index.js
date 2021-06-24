edb5339a311458ae257745abad441b6c5658c19e
Kathlene Tajonera <kathlene.tajonera@gmail.com>
2021-06-24 20:51:58 +0800

    desktop ui done

--- js/index.js
+++ js/index.js
@@ -4,10 +4,24 @@ const navToggle = document.querySelector(".nav__toggle");
 const creationsContainer = document.querySelector(
   ".creations__cards-container"
 );
+let creationsList = [];
+let windowWidth = window.innerWidth;
 
 navToggle.addEventListener("click", () => {
   nav.classList.toggle("nav--active");
   bodyLock();
+});
+
+window.addEventListener("resize", () => {
+  if (windowWidth !== window.innerWidth) {
+    windowWidth = window.innerWidth;
+
+    loadCreations();
+
+    if (windowWidth >= 1024) {
+      nav.classList.remove("nav--active");
+    }
+  }
 });
 
 function bodyLock() {
@@ -22,19 +36,23 @@ const fetchCreations = async () => {
   const response = await fetch("js/creationsList.json");
   const data = await response.json();
 
-  loadCreations(data);
+  creationsList = data;
+
+  loadCreations();
 };
 
 fetchCreations();
 
-function loadCreations(list) {
-  list.map((item) =>
+function loadCreations() {
+  creationsContainer.innerHTML = "";
+
+  creationsList.map((item) =>
     creationsContainer.insertAdjacentHTML(
       "beforeend",
       `
         <div class="creations__card">
             <img
-              src=${item.image.mobile}
+              src=${windowWidth < 768 ? item.image.mobile : item.image.desktop}
               alt=${item.name}
               class="creations__image"
             />
